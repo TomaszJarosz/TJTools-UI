2ba4e9ec7d792cce7386e771d4f204a0d227d0da
tomaszjarosz <tomaszjarosz1994@gmail.com>
2024-07-15 22:31:22 +0200

    remove Content.js const.js

--- src/ToolsApp.js
+++ src/ToolsApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes,} from "react-router-dom";
-import TopHeader from "./tools/header/TopHeader";
+import TopHeader from "./components/TopHeader";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient()
--- src/tools/const.js
+++
@@ -1,1 +0,0 @@
-export const leftToolbarWidth = 240;
